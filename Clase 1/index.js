73a3da83cafe8f7b0dfff8c845e601725e6fbc1e
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-04-07 19:13:47 -0500

    clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,55 @@
+//console.log("hola");
+
+/* 
+en esta parte del codigo hacemos los lalmadaos necesarios
+laksjhdfg
+alksjdfh
+lkajsdghf
+lkajshdf
+*/
+
+//Variables y valores, forma de declararlas
+/* let apellido = "urrea";
+console.log("1", apellido);
+apellido = "vela";
+console.log("2", apellido); */
+
+/* const nombre = "andres";
+nombre = "carmen";  */
+
+//var numero = 3;
+
+/* let nombre;
+
+nombre = "andres";
+
+console.log(nombre); */
+
+/* let numeroA = 1;
+let numeroB = 2;
+
+const NUMEROC = 3;
+//Suma  de dos números (1 + 2 = 3)
+let resultadoSuma = numeroA + numeroB;
+//Resta de dos números (2 - 1 = 1)
+let resultadoResta = numeroB - numeroA;
+//Producto de dos números (2 * 3 = 6)
+let resultadoProducto = numeroB * NUMEROC;
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoProducto); */
+
+/* let nombre = "Andres";
+let apellido = "Ruiz";
+
+console.log(nombre); */
+//typeof()  me dice el tipo de datos de la variable
+
+/* let edad = prompt("ingrese su edad");
+console.log(typeof(edad));
+alert(edad) */
+
+//parseInt
+
+/* console.log(parseFloat("4.5")); */
---
+++ Clase 1/segundo.js
@@ -0,0 +1,3 @@
+let entrada = prompt("Ingresar una letra");
+let salida  = entrada + " " + "ingresada";
+alert(salida);
