f6d217dc6fb616c75cc87a250df16b925dafba35
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-04-18 18:02:03 -0500

    after 2

---
+++ After Class 2/index.js
@@ -0,0 +1,18 @@
+/* let intentos = 0;
+
+do{
+  var usuario = prompt('ingrese el usuario');
+  var contrasena = prompt('Ingresa la contrasena');
+  intentos++;
+}while(contrasena != "1234" && intentos != 3)
+
+
+alert("estas bloqueado"); */
+
+
+/* while(contrasena != "1234" && intentos != 3){
+  intentos++;
+  contrasena = prompt('Ingresa la contrasena');
+}
+
+alert("estas bloqueado"); */
